0ad2b1f116d763ef81b346b1bea84b5612a3ead6
N-Pushpalatha <144149770+N-Pushpalatha@users.noreply.github.com>
2023-09-20 12:19:30 +0530

    Add files via upload

---
+++ season5.py
@@ -0,0 +1,11 @@
+month = input("Input the month : ")
+if month in ('september', 'october', 'november'):
+	print("The season is Autumn")
+elif month in ('december', 'january', 'february'):
+	print("The season is Winter")
+elif month in ('march', 'April', 'may'):
+	print("The season is spring")
+elif month in ('june', 'july', 'august'):
+	print("The season is summer")
+else:
+       print("invalid month")
---
+++ task5.py
@@ -0,0 +1,9 @@
+a=int(input('Enter the first no:'));
+b=int(input('Enter the second no:'));
+if a>b:
+  print("a is greater than b")
+elif a<b:
+ print("a is less than b")
+else:
+ print("a is equal to b")
+                                         
